4499966bf8f047188b71ad3cd5fffe917c3345a2
bobbylam <bobbyfreshh@gmail.com>
2014-04-22 02:27:50 -0700

    Added credit for template and maps styling

--- js/madebybobby.js
+++ js/madebybobby.js
@@ -25,6 +25,7 @@ var myOptions = {
     center: myCoords,
     mapTypeId: google.maps.MapTypeId.ROADMAP,
     disableDefaultUI: true,
+    // Thanks to snazzymaps.com for the map style below!
     styles: [{"featureType":"landscape","stylers":[{"saturation":-100},{"lightness":65},{"visibility":"on"}]},{"featureType":"poi","stylers":[{"saturation":-100},{"lightness":51},{"visibility":"off"}]},{"featureType":"road.highway","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"road.arterial","stylers":[{"saturation":-100},{"lightness":30},{"visibility":"on"}]},{"featureType":"road.local","stylers":[{"saturation":-100},{"lightness":40},{"visibility":"on"}]},{"featureType":"transit","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"administrative.province","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":-25},{"saturation":-100}]},{"featureType":"water","elementType":"geometry","stylers":[{"hue":"#ffff00"},{"lightness":-25},{"saturation":-97}]}]
 };
 
