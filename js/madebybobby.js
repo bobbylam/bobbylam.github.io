cd00ce8495e7797a986ff348e4e76a367ae8a659
Bobby Lam <bobbyfreshh@gmail.com>
2016-06-08 18:38:08 -0700

    Website 2.0
    Lots of changes.

--- js/madebybobby.js
+++ js/madebybobby.js
@@ -1,6 +1,7 @@
 //jQuery to collapse the navbar on scroll
 $(window).scroll(function() {
-    if ($(".navbar").offset().top > 50) {
+    // var $y = $(window).height();
+    if ($(".navbar").offset().top > 10) {
         $(".navbar-fixed-top").addClass("top-nav-collapse");
     } else {
         $(".navbar-fixed-top").removeClass("top-nav-collapse");
